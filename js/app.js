49004cae4cc49554a428e28e84a428d21a98b8b8
codingfacilties <120.practice.learning@gmail.com>
2022-04-28 17:04:56 +0700

    Apr 28, 2022

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 new WOW().init();
+
+
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    /////// Prevent closing from click inside dropdown
+    document.querySelectorAll('.dropdown-menu').forEach(function (element) {
+        element.addEventListener('click', function (e) {
+            e.stopPropagation();
+        });
+    })
+});
+// DOMContentLoaded  end
